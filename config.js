38d2a5fdca89b6cdb739ce92bdd523327f3be980
Luuk Weiss <luukweiss@gmail.com>
2025-11-28 21:59:30 +0100

    appeareatly firebase creds are not dangerious to expose

---
+++ config.js
@@ -0,0 +1,8 @@
+window.env = {
+  VITE_API_KEY: "AIzaSyBflwkHnZtXhlcOEhXutGd-tWriWvadqGU",
+  VITE_AUTH_DOMAIN: "leibgame-c7d34.firebaseapp.com",
+  VITE_PROJECT_ID: "leibgame-c7d34",
+  VITE_STORAGE_BUCKET: "leibgame-c7d34.firebasestorage.app",
+  VITE_MESSAGING_SENDER_ID: "118964504250",
+  VITE_APP_ID: "1:118964504250:web:63e25bb0512060acb82483"
+};
